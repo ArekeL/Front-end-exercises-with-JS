10d0b1fee3bdbfa26111c23b36060882f0d2eef4
Arek eL <alechnik@gmail.com>
2019-03-26 06:40:28 +0100

    Conditional function / if / else - small exercise
    Signed-off-by: Arek eL <alechnik@gmail.com>

--- Section 2/Exe 2/main.js
+++ Section 2/Exe 2/main.js
@@ -15,22 +15,25 @@ colorLine.style.left = '0';
 colorLine.style.top = '0';
 colorLine.style.position = 'fixed';
 
-function changeHeight() {
+const changeHeight = function () {
   if (heightSize >= window.innerHeight / 2) {
+    // grow = false;
     grow = !grow;
+    colorLine.style.backgroundColor = 'red';
   } else if (heightSize <= 0) {
-    grow = true;
+    // grow = true;
+    grow = !grow;
+    colorLine.style.backgroundColor = 'green';
   }
 
   if (grow == true) {
-    heightSize += 5
-    colorLine.style.height = heightSize + 'px';
-    colorLine.style.backgroundColor = 'green';
+    heightSize += 10
+    // colorLine.style.backgroundColor = 'green';
   } else {
-    heightSize -= 5;
-    colorLine.style.height = heightSize + 'px'
-    colorLine.style.backgroundColor = 'red';
+    heightSize -= 10;
+    // colorLine.style.backgroundColor = 'red';
   }
+  colorLine.style.height = heightSize + 'px';
 }
 
 window.addEventListener('scroll', changeHeight);
