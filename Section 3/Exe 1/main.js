5248072f9bd0a2208cae12258364a52d6ba08ee0
Arek eL <alechnik@gmail.com>
2019-03-27 06:23:46 +0100

    loop for and foreach exercise
    Signed-off-by: Arek eL <alechnik@gmail.com>

--- Section 3/Exe 1/main.js
+++ Section 3/Exe 1/main.js
@@ -1,16 +1,26 @@
 // Code
 
+//For
+
 const btn = document.querySelector('button');
 const liItems = document.querySelectorAll('li');
 let sizeFont = 10;
+// btn.addEventListener('click', () => {
+//   console.log('dziala');
+//   sizeFont++;
+//   for (let i = 0; i < liItems.length; i++) {
+//     liItems[i].style.display = 'block';
+//     liItems[i].style.fontSize = `${sizeFont}px`;
+//   }
+// })
+
+//Foreach
+
 btn.addEventListener('click', () => {
-  console.log('dziala');
-  sizeFont++;
-  for (let i = 0; i < liItems.length; i++) {
-    liItems[i].style.display = 'block';
-    liItems[i].style.fontSize = `${sizeFont}px`;
 
-  }
-
-
+  liItems.forEach((liItem) => {
+    liItem.style.display = 'block';
+    liItem.style.fontSize = sizeFont + 'px';
+  })
+  sizeFont++
 })
