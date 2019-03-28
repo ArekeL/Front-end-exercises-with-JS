0789aeca5498a8edd2785a9ee824efcffbaf1cde
Arek eL <alechnik@gmail.com>
2019-03-28 05:59:03 +0100

    Function exercise - in progres
    Signed-off-by: Arek eL <alechnik@gmail.com>

--- Section 3/Exe 2/main.js
+++ Section 3/Exe 2/main.js
@@ -1,12 +1,21 @@
 let size = 10;
 let orderElement = 1;
 
+
 const init = () => {
- // tutaj kod
+  const btn = document.createElement('button');
+  btn.textContent = '10 elements';
+  btn.style.fontSize = '20px';
+  document.body.appendChild(btn)
+  const ulElement = document.createElement('ul');
+  document.body.appendChild(ulElement);
+
+  btn.addEventListener('click', createLiElements);
 }
 
 const createLiElements = () => {
- // tutaj kod
+  console.log('Dziala function 2');
+
 }
 
 init()
