2f7ec0c442f958c1398a15b2e2e95a4277668aa7
Arek eL <alechnik@gmail.com>
2019-04-02 05:23:40 +0200

    creatElement / eventListener / loop - exercise
    Signed-off-by: Arek eL <alechnik@gmail.com>

--- Section 3/Exe 2/main.js
+++ Section 3/Exe 2/main.js
@@ -4,18 +4,35 @@ let orderElement = 1;
 
 const init = () => {
   const btn = document.createElement('button');
+  const btnReset = document.createElement('button');
   btn.textContent = '10 elements';
+  btnReset.textContent = 'Reset';
   btn.style.fontSize = '20px';
+  btnReset.style.fontSize = '20px';
   document.body.appendChild(btn)
+  document.body.appendChild(btnReset)
   const ulElement = document.createElement('ul');
+  ulElement.style.listStyle = 'none'
   document.body.appendChild(ulElement);
 
   btn.addEventListener('click', createLiElements);
+  btnReset.addEventListener('click', cleanList);
 }
 
 const createLiElements = () => {
-  console.log('Dziala function 2');
+  for (let i = 0; i < 10; i++) {
+    const li = document.createElement('li')
+    li.textContent = `Element nr ${orderElement++}`;
+    li.style.fontSize = `${size++}px`;
+    document.querySelector('ul').appendChild(li);
+  }
 
+}
+
+const cleanList = () => {
+  document.querySelector('ul').textContent = '';
+  size = 10;
+  orderElement = 1;
 }
 
 init()
