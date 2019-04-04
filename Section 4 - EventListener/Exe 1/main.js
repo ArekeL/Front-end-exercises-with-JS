cd83858ab85c15905b54b398cdd7b4a14285cdb1
Arek eL <alechnik@gmail.com>
2019-04-04 05:08:25 +0200

    EventListener / elements - exercise
    Signed-off-by: Arek eL <alechnik@gmail.com>

--- Section 4 - EventListener/Exe 1/main.js
+++ Section 4 - EventListener/Exe 1/main.js
@@ -1,13 +1,31 @@
-//
+// Option 1
+// const h1 = document.querySelector('h1');
+
+// document.body.addEventListener('mousemove', function (e) {
+//   //h1.textContent = `${e.screenX}, ${e.screenY}`;
+//   //h1.textContent = `${e.pageX}, ${e.pageY}`;
+//   h1.textContent = `${e.clientX}, ${e.clientY}`;
+
+//   document.body.style.backgroundColor = `rgb(${e.clientX/3}, ${e.clientY/2}, ${e.clientX/e.clientY * 2})`;
+//   // document.body.style.backgroundColor = 'rgb(' + e.clientX / 3 + ', ' + e.clientY / 2 + ', ' + e.clientX / e.clientY * 2 + ')';
+
+
+// })
+
+//Option 2
+
 const h1 = document.querySelector('h1');
-
 document.body.addEventListener('mousemove', function (e) {
-  //h1.textContent = `${e.screenX}, ${e.screenY}`;
-  //h1.textContent = `${e.pageX}, ${e.pageY}`;
-  h1.textContent = `${e.clientX}, ${e.clientY}`;
+  const x = e.clientX + 1;
+  const y = e.clientY + 1;
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  h1.textContent = `${x}, ${y}`;
 
-  document.body.style.backgroundColor = `rgb(${e.clientX/3}, ${e.clientY/2}, ${e.clientX/e.clientY * 2})`;
-  // document.body.style.backgroundColor = 'rgb(' + e.clientX / 3 + ', ' + e.clientY / 2 + ', ' + e.clientX / e.clientY * 2 + ')';
+  const red = x / width * 100;
+  const green = y / height * 100;
+  const blue = ((x / width * 100) + (y / height * 100)) / 2;
 
+  document.body.style.backgroundColor = `rgb(${red}%, ${green}%, ${blue}%)`
 
 })
